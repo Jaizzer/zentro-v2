d75ac6485c421157e74ab807b2858a55916e0e0a
Jaizzer <florenzjaizzer.calderon@gmail.com>
2025-09-19 14:48:47 +0800

    style: Format code

--- backend/app.ts
+++ backend/app.ts
@@ -22,7 +22,10 @@ app.listen(PORT, (error) => {
 	if (error) {
 		console.error(`Failed to start server: ${error}`);
 	} else {
-		const baseUrl = process.env.NODE_ENVIRONMENT === 'PRODUCTION' ? process.env.PRODUCTION_URL : `http://localhost:${PORT}`;
+		const baseUrl =
+			process.env.NODE_ENVIRONMENT === 'PRODUCTION'
+				? process.env.PRODUCTION_URL
+				: `http://localhost:${PORT}`;
 		console.log(`Server is listening on: ${baseUrl}`);
 	}
 });
