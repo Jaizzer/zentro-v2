be02420035ec78ab6486e90a856fef8eb4fa336b
Jaizzer <florenzjaizzer.calderon@gmail.com>
2025-09-19 10:27:26 +0800

    style: Format code

--- backend/app.ts
+++ backend/app.ts
@@ -1,17 +1,17 @@
 // Setup the server
-import express from "express";
+import express from 'express';
 
 // Import dependencies for working with files and directory paths
-import path from "path";
-import { fileURLToPath } from "url";
+import path from 'path';
+import { fileURLToPath } from 'url';
 
 // Get the current file directory path
 const __fileName = fileURLToPath(import.meta.url);
 const __dirName = path.dirname(__fileName);
 
 // Load the environment variables
-import dotenv from "dotenv";
-dotenv.config({ path: path.resolve(__dirName, "../.env") });
+import dotenv from 'dotenv';
+dotenv.config({ path: path.resolve(__dirName, '../.env') });
 
 // Load the port
 const PORT = process.env.PORT || 8080;
@@ -22,10 +22,7 @@ app.listen(PORT, (error) => {
 	if (error) {
 		console.error(`Failed to start server: ${error}`);
 	} else {
-		const baseUrl =
-			process.env.NODE_ENVIRONMENT === "PRODUCTION"
-				? process.env.PRODUCTION_URL
-				: `http://localhost:${PORT}`;
+		const baseUrl = process.env.NODE_ENVIRONMENT === 'PRODUCTION' ? process.env.PRODUCTION_URL : `http://localhost:${PORT}`;
 		console.log(`Server is listening on: ${baseUrl}`);
 	}
 });
