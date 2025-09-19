ac959ecef8844b81ca2c5c40084ea45eaf610e19
Jaizzer <florenzjaizzer.calderon@gmail.com>
2025-09-19 10:10:33 +0800

    feat: Ensure port availability when running the server

--- backend/app.ts
+++ backend/app.ts
@@ -1,8 +1,23 @@
 // Setup the server
 import express from "express";
 
+// Import dependencies for working with files and directory paths
+import path from "path";
+import { fileURLToPath } from "url";
+
+// Get the current file directory path
+const __fileName = fileURLToPath(import.meta.url);
+const __dirName = path.dirname(__fileName);
+
+// Load the environment variables
+import dotenv from "dotenv";
+dotenv.config({ path: path.resolve(__dirName, "../.env") });
+
+// Load the port
+const PORT = process.env.PORT || 8080;
+
+// Initialize the application
 const app = express();
-const PORT = process.env.PORT || 5000;
 app.listen(PORT, (error) => {
 	if (error) {
 		console.error(`Failed to start server: ${error}`);
