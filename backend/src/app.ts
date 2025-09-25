d75f6c1f4427aff31a0d92f7069f165faecea5d1
Jaizzer <florenzjaizzer.calderon@gmail.com>
2025-09-25 10:28:34 +0800

    feat: Use the index router in the app for route "/"

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,6 +1,10 @@
 import express from 'express';
 
+import indexRouter from './routers/index.router.ts';
+
 // Initialize the application
 const app = express();
+
+app.use('/', indexRouter);
 
 export default app;
