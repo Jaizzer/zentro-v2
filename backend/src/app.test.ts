3f5f1e9b7326279ef8cc9a235f291c8792841471
Jaizzer <florenzjaizzer.calderon@gmail.com>
2025-09-25 10:31:51 +0800

    chore: Create initial test file

---
+++ backend/src/app.test.ts
@@ -0,0 +1,10 @@
+import request from 'supertest';
+
+import app from './app.ts';
+
+describe('Index', () => {
+	it('responds with 200', async () => {
+		const response = await request(app).get('/');
+		expect(response.status).toEqual(200);
+	});
+});
