d56f947eb00cfba827834fda58e04b8e2ad1197f
Jaizzer <florenzjaizzer.calderon@gmail.com>
2025-09-28 10:23:27 +0800

    chore: Test github workflow

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -5,6 +5,8 @@ import indexRouter from './routers/index.router.ts';
 // Initialize the application
 const app = express();
 
+console.log('Test');
+
 app.use('/', indexRouter);
 
 export default app;
