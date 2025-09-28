5cb68032bea94b1d25f58ee2b6ed709d2698312a
Jaizzer <florenzjaizzer.calderon@gmail.com>
2025-09-28 11:03:06 +0800

    cleanup: Remove console.log

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -5,8 +5,6 @@ import indexRouter from './routers/index.router.ts';
 // Initialize the application
 const app = express();
 
-console.log('Test');
-
 app.use('/', indexRouter);
 
 export default app;
