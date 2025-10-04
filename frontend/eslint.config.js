7fc5db80d661acbd7bc055a40397fb77a5f7b05b
Jaizzer <florenzjaizzer.calderon@gmail.com>
2025-10-04 09:07:02 +0800

    chore: Update linting configurations

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -1,24 +1,29 @@
 import eslint from "@eslint/js";
-import { defineConfig, globalIgnores } from "eslint/config";
+import { defineConfig } from "eslint/config";
 import tseslint from "typescript-eslint";
-
+import perfectionist from "eslint-plugin-perfectionist";
 import globals from "globals";
 import reactHooks from "eslint-plugin-react-hooks";
 import reactRefresh from "eslint-plugin-react-refresh";
 
 export default defineConfig([
-	globalIgnores(["dist"]),
 	{
-		files: ["**/*.{ts,tsx}"],
-		extends: [
-			eslint.configs.recommended,
-			tseslint.configs.recommended,
-			reactHooks.configs["recommended-latest"],
-			reactRefresh.configs.vite,
-		],
+		ignores: ["**/*.js"],
+	},
+	eslint.configs.recommended,
+	tseslint.configs.strictTypeChecked,
+	tseslint.configs.stylisticTypeChecked,
+	reactHooks.configs["recommended-latest"],
+	reactRefresh.configs.vite,
+	{
 		languageOptions: {
 			ecmaVersion: 2020,
 			globals: globals.browser,
 		},
+		parserOptions: {
+			projectService: true,
+			tsconfigRootDir: import.meta.dirname,
+		},
 	},
+	perfectionist.configs["recommended-natural"],
 ]);
