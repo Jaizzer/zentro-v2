8acb4cd41c615c82d0e76a0033ab9f8aa4cf3f0b
Jaizzer <florenzjaizzer.calderon@gmail.com>
2025-10-04 09:24:17 +0800

    feat: Throw an error whenever the root is undefined to prevent the use of no null assertion

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,8 +3,18 @@ import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App.tsx';
 
-createRoot(document.getElementById('root')!).render(
+createRoot(getRoot('root')).render(
 	<StrictMode>
 		<App />
 	</StrictMode>
 );
+
+function getRoot(elementTag: string): HTMLElement {
+	const root = document.getElementById(elementTag);
+
+	if (!root) {
+		throw new Error(`Root does not exist.'].`);
+	}
+
+	return root;
+}
