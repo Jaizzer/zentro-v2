8f4369453c96fd7b6dcb3cf0df514fbbf9e605ae
Jaizzer <florenzjaizzer.calderon@gmail.com>
2025-10-12 23:32:18 +0800

    debug: Add initila html template for vercel deployment

--- backend/src/controllers/index.controller.ts
+++ backend/src/controllers/index.controller.ts
@@ -1,6 +1,6 @@
 import { Request, Response } from 'express';
 function getIndex(req: Request, res: Response) {
-	return res.status(200).send();
+	return res.send('<h1>Test Deployment</h1>');
 }
 
 export default {
