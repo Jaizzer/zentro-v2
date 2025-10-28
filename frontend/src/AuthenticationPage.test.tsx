e8f7bb50bcf193d24d3eca12e31ed8d6035a4712
Jaizzer <florenzjaizzer.calderon@gmail.com>
2025-10-28 13:29:11 +0800

    test: Check whether the sign up button click redirects to the sign up page

--- frontend/src/AuthenticationPage.test.tsx
+++ frontend/src/AuthenticationPage.test.tsx
@@ -1,4 +1,5 @@
 import { render, screen } from '@testing-library/react';
+import { userEvent } from '@testing-library/user-event';
 import { MemoryRouter } from 'react-router-dom';
 import { describe, expect, it } from 'vitest';
 
@@ -49,5 +50,27 @@ describe('Authentication page', () => {
 		);
 		const signInButton: HTMLElement | null = screen.queryByText('Sign In');
 		expect(signInButton).not.toBeNull();
+	});
+
+	it('redirects to the sign up page when the Sign Up button is clicked', async () => {
+		// Render the authentication page component
+		render(
+			<MemoryRouter initialEntries={['/']}>
+				<AuthenticationPage />
+			</MemoryRouter>
+		);
+
+		// Initialize the user
+		const user = userEvent.setup();
+
+		// Simulate sign up button click
+		const signUpButton: HTMLElement | null = screen.queryByText('Sign Up');
+		if (signUpButton) {
+			await user.click(signUpButton);
+		}
+
+		const registerPageHeading = screen.findByText('Register');
+
+		expect(registerPageHeading).not.toBeNull();
 	});
 });
