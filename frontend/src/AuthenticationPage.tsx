a782ecc7fd31615779dc6ef444a05f638b3f9ddc
Jaizzer <florenzjaizzer.calderon@gmail.com>
2025-10-28 13:24:21 +0800

    feat: Replace the buttons with Link component

--- frontend/src/AuthenticationPage.tsx
+++ frontend/src/AuthenticationPage.tsx
@@ -1,12 +1,18 @@
 export default function AuthenticationPage() {
+	import { Link } from 'react-router';
+
 	return (
 		<>
 			<h1>Welcome to Zentro!</h1>
 			<p>Zentro is an easy to use cloud storage.</p>
 
 			<span className="actions">
-				<button className="signUp">Sign Up</button>
-				<button className="signIn">Sign In</button>
+				<Link className="signUp" to="/sign-up">
+					Sign Up
+				</Link>
+				<Link className="signIn" to="/sign-in">
+					Sign In
+				</Link>
 			</span>
 		</>
 	);
