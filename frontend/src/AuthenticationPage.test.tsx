d24926417b1e2ac51dfa05d00a71f038f712ecc3
Jaizzer <florenzjaizzer.calderon@gmail.com>
2025-10-28 13:27:23 +0800

    test: Wrapped the AuthenticationPage component inside a memory router to support the newly added routing logic

--- frontend/src/AuthenticationPage.test.tsx
+++ frontend/src/AuthenticationPage.test.tsx
@@ -1,17 +1,28 @@
 import { render, screen } from '@testing-library/react';
+import { MemoryRouter } from 'react-router-dom';
 import { describe, expect, it } from 'vitest';
 
 import AuthenticationPage from './AuthenticationPage';
 
 describe('Authentication page', () => {
 	it('renders the welcome message', () => {
-		render(<AuthenticationPage />);
+		// Render the authentication page component
+		render(
+			<MemoryRouter>
+				<AuthenticationPage />
+			</MemoryRouter>
+		);
 		const welcomeMessage: HTMLElement | null = screen.queryByText('Welcome to Zentro!');
 		expect(welcomeMessage).not.toBeNull();
 	});
 
 	it('renders the app description', () => {
-		render(<AuthenticationPage />);
+		// Render the authentication page component
+		render(
+			<MemoryRouter>
+				<AuthenticationPage />
+			</MemoryRouter>
+		);
 		const description: HTMLElement | null = screen.queryByText(
 			'Zentro is an easy to use cloud storage.'
 		);
@@ -19,13 +30,23 @@ describe('Authentication page', () => {
 	});
 
 	it('renders the sign up button', () => {
-		render(<AuthenticationPage />);
+		// Render the authentication page component
+		render(
+			<MemoryRouter>
+				<AuthenticationPage />
+			</MemoryRouter>
+		);
 		const signUpButton: HTMLElement | null = screen.queryByText('Sign Up');
 		expect(signUpButton).not.toBeNull();
 	});
 
 	it('renders the sign in button', () => {
-		render(<AuthenticationPage />);
+		// Render the authentication page component
+		render(
+			<MemoryRouter>
+				<AuthenticationPage />
+			</MemoryRouter>
+		);
 		const signInButton: HTMLElement | null = screen.queryByText('Sign In');
 		expect(signInButton).not.toBeNull();
 	});
