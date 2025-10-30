3e0ccc3ca024518bcce9e581f888eb4131773707
Jaizzer <florenzjaizzer.calderon@gmail.com>
2025-10-30 20:24:53 +0800

    feat: Create initial styling to the link component

--- frontend/src/AuthenticationPage.tsx
+++ frontend/src/AuthenticationPage.tsx
@@ -1,6 +1,22 @@
 import type { JSX } from 'react';
 
 import { Link } from 'react-router';
+import styled, { css } from 'styled-components';
+
+const NavLink = styled(Link)<{ $button?: boolean }>`
+	color: black;
+	text-decoration: none;
+	font-family: 'Arial';
+
+	${(props) =>
+		props.$button &&
+		css`
+			padding: 10px;
+			border-radius: 7px;
+			background-color: #6161ff;
+			color: white;
+		`};
+`;
 
 export default function AuthenticationPage(): JSX.Element {
 	return (
@@ -9,12 +25,12 @@ export default function AuthenticationPage(): JSX.Element {
 			<p>Zentro is an easy to use cloud storage.</p>
 
 			<span className="actions">
-				<Link className="signUp" to="/sign-up">
+				<NavLink $button className="signUp" to="/sign-up">
 					Sign Up
-				</Link>
-				<Link className="signIn" to="/sign-in">
+				</NavLink>
+				<NavLink $button className="signIn" to="/sign-in">
 					Sign In
-				</Link>
+				</NavLink>
 			</span>
 		</>
 	);
