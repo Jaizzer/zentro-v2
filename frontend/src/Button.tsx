0c7cc813944902c820a95f44a1673642696c838b
Jaizzer <florenzjaizzer.calderon@gmail.com>
2025-11-05 05:11:08 +0800

    feat: Create initial button component

---
+++ frontend/src/Button.tsx
@@ -0,0 +1,20 @@
+import type { JSX } from 'react';
+
+import styled from 'styled-components';
+
+const StyledButton = styled.button`
+	padding: 0.75rem;
+	border-radius: 0.5rem;
+	background-color: var(--primary);
+	color: white;
+
+	&:hover {
+		background-color: var(--accent);
+	}
+`;
+
+type onClickCallback = () => void;
+
+export default function Button(onClick: onClickCallback, textContent: string): JSX.Element {
+	return <StyledButton onClick={onClick}>{textContent}</StyledButton>;
+}
