f868be68fe6d2a2be8d46c503e819a981df00758
Jaizzer <florenzjaizzer.calderon@gmail.com>
2025-11-11 07:26:30 +0800

    ref: Remove non-existent import

--- frontend/src/AuthenticationPage.tsx
+++ frontend/src/AuthenticationPage.tsx
@@ -1,12 +1,10 @@
 import type { JSX } from 'react';
 
-import MontserratFontStyle from './GlobalStyle';
 import NavLink from './NavLink';
 
 export default function AuthenticationPage(): JSX.Element {
 	return (
 		<>
-			<MontserratFontStyle />
 			<h1>Welcome to Zentro!</h1>
 			<p>Zentro is an easy to use cloud storage.</p>
 
