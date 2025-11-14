eeafd56a3c86ec67d18e7a5d0231c96eae82a796
Jaizzer <florenzjaizzer.calderon@gmail.com>
2025-11-14 09:31:37 +0800

    feat: Add initial styling to the heading

--- frontend/src/AuthenticationPage.tsx
+++ frontend/src/AuthenticationPage.tsx
@@ -5,7 +5,7 @@ import { Link } from 'react-router';
 export default function AuthenticationPage(): JSX.Element {
 	return (
 		<>
-			<h1>Welcome to Zentro!</h1>
+			<h1 className="text-3xl font-bold">Welcome to Zentro!</h1>
 			<p>Zentro is an easy to use cloud storage.</p>
 
 			<span className="actions">
