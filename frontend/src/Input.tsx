869945d182f154c4cf34b960c80915b0393996c7
Jaizzer <florenzjaizzer.calderon@gmail.com>
2025-11-25 05:29:58 +0800

    feat: Add label to the input

--- frontend/src/Input.tsx
+++ frontend/src/Input.tsx
@@ -9,12 +9,15 @@ export interface InputProps {
 
 export default function Input({ id, name, placeholder, type }: InputProps): JSX.Element {
 	return (
-		<input
-			className="outline-accent rounded-md bg-gray-100 px-4 py-3"
-			data-testid={name}
-			id={id}
-			placeholder={placeholder}
-			type={type}
-		/>
+		<div className="grid w-full">
+			<label htmlFor={id}>{name}</label>
+			<input
+				className="outline-accent rounded-md bg-gray-100 px-4 py-3"
+				data-testid={name}
+				id={id}
+				placeholder={placeholder}
+				type={type}
+			/>
+		</div>
 	);
 }
