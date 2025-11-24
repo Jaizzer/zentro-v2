1d8816845d1881946d032991e296f698620588c1
Jaizzer <florenzjaizzer.calderon@gmail.com>
2025-11-24 09:42:56 +0800

    feat: Put additional props to the input component

--- frontend/src/Input.test.tsx
+++ frontend/src/Input.test.tsx
@@ -1,12 +1,19 @@
 import { render, screen } from '@testing-library/react';
 import { describe, expect, it } from 'vitest';
 
-import Input from './Input';
+import Input, { type InputProps } from './Input';
+
+const inputProps: InputProps = {
+	id: 'id',
+	name: 'input',
+	placeholder: 'placeholde',
+	type: 'email',
+};
 
 describe('Input', () => {
 	it('renders an input element', () => {
 		// Render the Input component with name 'input'
-		render(<Input name={'input'} />);
+		render(<Input {...inputProps} />);
 
 		// Find the input with the name 'input'
 		const input: HTMLElement = screen.getByTestId(/input/i);
--- frontend/src/Input.tsx
+++ frontend/src/Input.tsx
@@ -1,9 +1,12 @@
 import type { JSX } from 'react';
 
-interface InputProps {
+export interface InputProps {
+	id: string;
 	name: string;
+	placeholder: string;
+	type: string;
 }
 
-export default function Input({ name }: InputProps): JSX.Element {
-	return <input data-testid={name} />;
+export default function Input({ id, name, placeholder, type }: InputProps): JSX.Element {
+	return <input data-testid={name} id={id} placeholder={placeholder} type={type} />;
 }
