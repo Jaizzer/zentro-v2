cd8768e48563021118d42fbce5bd8d92a84e70f8
Jaizzer <florenzjaizzer.calderon@gmail.com>
2025-11-27 13:36:37 +0800

    feat: Add initial password visibility button toggle function

--- frontend/src/Input.test.tsx
+++ frontend/src/Input.test.tsx
@@ -1,10 +1,12 @@
 import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import { describe, expect, it } from 'vitest';
 
 import Input, { type InputProps } from './Input';
 
 const inputProps: InputProps = {
 	id: 'id',
+	isVisibilityToggleButtonEnabled: false,
 	name: 'input',
 	placeholder: 'placeholder',
 	type: 'email',
@@ -16,8 +18,90 @@ describe('Input', () => {
 		render(<Input {...inputProps} />);
 
 		// Find the input with the name 'input'
-		const input: HTMLElement = screen.getByTestId(/input/i);
+		const input: HTMLInputElement = screen.getByTestId('input');
 
 		expect(input).not.toBeNull();
+	});
+
+	it('initially hides the input value when the visibility toggle button is enabled', () => {
+		// Render the Input component
+		render(<Input {...inputProps} isVisibilityToggleButtonEnabled={true} type="password" />);
+
+		// Find the input with the name 'input'
+		const input: HTMLInputElement = screen.getByTestId('input');
+
+		expect(input.type).toEqual('password');
+	});
+
+	it('renders the show input value button when the visibility toggle button is enabled', () => {
+		// Render the Input component
+		render(<Input {...inputProps} isVisibilityToggleButtonEnabled={true} type="password" />);
+
+		// Find the show input value button
+		const showInputValueButton: HTMLElement = screen.getByTestId(/show-input-value-button/i);
+
+		expect(showInputValueButton).toBeTruthy();
+	});
+
+	it('does not render the show input value button when the visibility toggle button is disabled ', () => {
+		// Render the Input component
+		render(<Input {...inputProps} isVisibilityToggleButtonEnabled={false} type="password" />);
+
+		// Find the show input value button
+		const showInputValueButton: HTMLElement | null =
+			screen.queryByTestId(/show-input-value-button/i);
+
+		expect(showInputValueButton).toBeNull();
+	});
+
+	it('shows the input value when the show input value button is clicked', async () => {
+		// Render the Input component
+		render(<Input {...inputProps} isVisibilityToggleButtonEnabled={true} type="password" />);
+
+		// Find the input with
+		const input: HTMLInputElement = screen.getByTestId('input');
+
+		// Get the initial input type
+		const initialInputType: string = input.type;
+
+		// Create the user
+		const user = userEvent.setup();
+
+		// Find the show input value button
+		const showInputValueButton: HTMLElement = screen.getByTestId(/show-input-value-button/i);
+
+		// Show the input value
+		await user.click(showInputValueButton);
+
+		// Get the new input type
+		const newInputType: string = input.type;
+
+		expect(newInputType).toEqual(initialInputType);
+	});
+
+	it('hides the input value when the hide input value button is clicked', async () => {
+		// Render the Input component
+		render(<Input {...inputProps} isVisibilityToggleButtonEnabled={true} type="text" />);
+
+		// Find the input with
+		const input: HTMLInputElement = screen.getByTestId('input');
+
+		// Create the user
+		const user = userEvent.setup();
+
+		// Show the input value first
+		const showInputValueButton: HTMLElement = screen.getByTestId(/show-input-value-button/i);
+		await user.click(showInputValueButton);
+
+		// Find the hide input value button
+		const hideInputValueButton: HTMLElement = screen.getByTestId(/hide-input-value-button/i);
+
+		// Hide the input value
+		await user.click(hideInputValueButton);
+
+		// Get the new input type
+		const newInputType: string = input.type;
+
+		expect(newInputType).toEqual('password');
 	});
 });
--- frontend/src/Input.tsx
+++ frontend/src/Input.tsx
@@ -1,13 +1,54 @@
-import type { JSX } from 'react';
+import { type JSX, useState } from 'react';
 
 export interface InputProps {
 	id: string;
+	isVisibilityToggleButtonEnabled?: boolean;
 	name: string;
 	placeholder: string;
 	type: string;
 }
 
-export default function Input({ id, name, placeholder, type }: InputProps): JSX.Element {
+export default function Input({
+	id,
+	isVisibilityToggleButtonEnabled,
+	name,
+	placeholder,
+	type,
+}: InputProps): JSX.Element {
+	const [isInputValueVisible, setIsInputValueVisible] = useState(
+		!isVisibilityToggleButtonEnabled
+	);
+
+	const [inputValue, setInputValue] = useState('');
+
+	let inputValueVisibilityButton: JSX.Element = (
+		<button
+			data-testid={'hide-input-value-button'}
+			onClick={() => {
+				setIsInputValueVisible(!isInputValueVisible);
+			}}
+			type="button"
+		>
+			Hide
+		</button>
+	);
+
+	if (!isInputValueVisible) {
+		inputValueVisibilityButton = (
+			<button
+				data-testid={'show-input-value-button'}
+				onClick={() => {
+					setIsInputValueVisible(!isInputValueVisible);
+				}}
+				type="button"
+			>
+				Show
+			</button>
+		);
+	}
+
+	console.log(inputValue);
+
 	return (
 		<div className="grid w-full">
 			<label htmlFor={id}>{name}</label>
@@ -15,9 +56,14 @@ export default function Input({ id, name, placeholder, type }: InputProps): JSX.
 				className="outline-accent rounded-md bg-gray-100 px-4 py-3"
 				data-testid={name}
 				id={id}
+				onChange={(event) => {
+					setInputValue(event.target.value);
+				}}
 				placeholder={placeholder}
-				type={type}
+				type={isInputValueVisible ? type : 'password'}
+				value={inputValue}
 			/>
+			{isVisibilityToggleButtonEnabled && inputValueVisibilityButton}
 		</div>
 	);
 }
